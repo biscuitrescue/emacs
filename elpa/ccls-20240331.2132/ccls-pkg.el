8b10401fd026edfdef2857c5f69d99fa2743d304
biscuitrescue <karttikeyasinha11@gmail.com>
2024-12-30 22:51:23 +0530

    rust lsp

---
+++ elpa/ccls-20240331.2132/ccls-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ccls" "20240331.2132"
+  "Ccls client for lsp-mode."
+  '((emacs    "27.1")
+    (lsp-mode "6.3.1")
+    (dash     "2.14.1"))
+  :url "https://github.com/emacs-lsp/emacs-ccls"
+  :commit "9c91aad768d5c401295c79f341c5296b69b29490"
+  :revdesc "9c91aad768d5"
+  :keywords '("languages" "lsp" "c++"))
