7bd65cf2b1b349054983ca96926f8eb7045abb6c
biscuitrescue <karttikeyasinha11@gmail.com>
2025-10-20 03:40:18 +0530

    emacs ups

--- init.el
+++ init.el
@@ -2,3 +2,18 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("6b68dac76f392f443d823a745917c4d2d41db5d47719858963efc2dafca6cac4"
+     "fa63a24332bf5d6ae4410fadaa9a176ab7c3297ea36e3fd637314aa25dcfb74e"
+     default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
