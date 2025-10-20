a321fe8d57995a35135e3e81254c4b627239c060
biscuitrescue <karttikeyasinha11@gmail.com>
2025-10-20 10:15:42 +0530

    lsp

--- init.el
+++ init.el
@@ -8,13 +8,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("204d47d5330864ffd7b6fb51f371622604304bab762ed155874efc23910adf12"
+   '("3061706fa92759264751c64950df09b285e3a2d3a9db771e99bcbb2f9b470037"
+     "b27e4fb6e4de0f1b2543a34d7a8996bac0f90e7a5b8f3861acc26e4891f5f727"
+     "204d47d5330864ffd7b6fb51f371622604304bab762ed155874efc23910adf12"
      "63c0c03289dd6eae10d91a944d67a6364c72345f9cc6566f374a8953a3ee4019"
      "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
      "5ea908ced38d8051749cc45f67caad0cef759f602abcc1d4c9c1a9f4084a8bb5"
      "6b68dac76f392f443d823a745917c4d2d41db5d47719858963efc2dafca6cac4"
      "fa63a24332bf5d6ae4410fadaa9a176ab7c3297ea36e3fd637314aa25dcfb74e"
-     default)))
+     default))
+ '(package-selected-packages '(magit transient)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
