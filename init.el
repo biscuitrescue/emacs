6b20dbd908c17f9e862c81afb49f29b297c05ddb
biscuitrescue <karttikeyasinha11@gmail.com>
2025-10-21 17:03:02 +0530

    working emacs

--- init.el
+++ init.el
@@ -24,7 +24,7 @@
      "6b68dac76f392f443d823a745917c4d2d41db5d47719858963efc2dafca6cac4"
      "fa63a24332bf5d6ae4410fadaa9a176ab7c3297ea36e3fd637314aa25dcfb74e"
      default))
- '(package-selected-packages '(transient)))
+ '(package-selected-packages '(llama magit nix-mode transient writegood-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
